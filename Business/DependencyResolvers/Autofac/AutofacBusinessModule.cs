14fb8745be4f550e39af077e519274fa4038921e
=burhan-xD <=burhanozsahinoglu@windowslive.com>
2023-11-22 05:46:00 +0300

    autofac aop is added

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using Business.Abstracts;
+using Business.Concretes;
+using DataAccess.Abstracts;
+using DataAccess.Concretes.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CourseManager>().As<ICourseService>().SingleInstance();
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<InstructorManager>().As<IInstructorService>().SingleInstance();
+            builder.RegisterType<CourseInstructorManager>().As<ICourseInstructorService>().SingleInstance();
+
+            builder.RegisterType<EfCourseDal>().As<ICourseDal>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+            builder.RegisterType<EfInstructorDal>().As<IInstructorDal>().SingleInstance();
+            builder.RegisterType<EfCourseInstructorDal>().As<ICourseInstructorDal>().SingleInstance();
+
+        }
+    }
+}
+//builder.Services.AddSingleton<ICourseService, CourseManager>();
+//builder.Services.AddSingleton<ICourseDal, EfCourseDal>();
+
+//builder.Services.AddSingleton<ICategoryService, CategoryManager>();
+//builder.Services.AddSingleton<ICategoryDal, EfCategoryDal>();
+
+//builder.Services.AddSingleton<IInstructorService, InstructorManager>();
+//builder.Services.AddSingleton<IInstructorDal, EfInstructorDal>();
+
+//builder.Services.AddSingleton<ICourseInstructorService, CourseInstructorManager>();
+//builder.Services.AddSingleton<ICourseInstructorDal, EfCourseInstructorDal>();
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstracts;
 using Business.Concretes;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstracts;
 using DataAccess.Concretes.EntityFramework;
 
@@ -8,18 +11,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<ICourseService, CourseManager>();
-builder.Services.AddSingleton<ICourseDal, EfCourseDal>();
+/* Default AOP
+//builder.Services.AddSingleton<ICourseService, CourseManager>();
+//builder.Services.AddSingleton<ICourseDal, EfCourseDal>();
 
-builder.Services.AddSingleton<ICategoryService, CategoryManager>();
-builder.Services.AddSingleton<ICategoryDal, EfCategoryDal>();
+//builder.Services.AddSingleton<ICategoryService, CategoryManager>();
+//builder.Services.AddSingleton<ICategoryDal, EfCategoryDal>();
 
-builder.Services.AddSingleton<IInstructorService, InstructorManager>();
-builder.Services.AddSingleton<IInstructorDal, EfInstructorDal>();
+//builder.Services.AddSingleton<IInstructorService, InstructorManager>();
+//builder.Services.AddSingleton<IInstructorDal, EfInstructorDal>();
 
-builder.Services.AddSingleton<ICourseInstructorService, CourseInstructorManager>();
-builder.Services.AddSingleton<ICourseInstructorDal, EfCourseInstructorDal>();
+//builder.Services.AddSingleton<ICourseInstructorService, CourseInstructorManager>();
+//builder.Services.AddSingleton<ICourseInstructorDal, EfCourseInstructorDal>();
+*/
 
+//Autofac AOP
+builder.Host.UseServiceProviderFactory(services => new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => { builder.RegisterModule(new AutofacBusinessModule()); });
 
 var app = builder.Build();
 
